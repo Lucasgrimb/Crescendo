1a6ca9a15a14142934e68b9d0bacb9c429a0fa95
FrancoFelstinsky <francofelstinsky@gmail.com>
2023-08-16 10:41:18 -0300

    Arranque la pantalla register

--- Dj's view/Hello page.js
+++ Dj's view/Hello page.js
@@ -1,8 +1,39 @@
+// Selecciono los elementos que se animarán//
+const title = document.querySelector('.header-gradient');
+const description = document.querySelector('.color-gray');
+const form = document.querySelector('form');
+const button = document.querySelector('.login-cta');
+const nav = document.querySelector('.nav')
+const usuario = document.getElementById("usuario")
+usuario.addEventListener("click", async (e)=> {
+  const req = await fetch("")
+})
 
-function redirectToLogin() {
-    window.location.href = "Login.html";
-}
+// Configurar la animación//
+const animation = anime.timeline({
+  easing: 'easeOutExpo',
+  duration: 500,
+});
 
-function redirectToRegister() {
-    window.location.href = "Register.html";
-}
+animation
+  .add({
+    targets: nav,
+    translateX: [-50, 0],
+    opacity: [0, 1],
+  })
+  .add({
+    targets: title,
+    translateY: [-50, 0],
+    opacity: [0, 1],
+  })
+  .add({
+    targets: description,
+    translateY: [20, 0],
+    opacity: [0, 1],
+  })
+  .add({
+    targets: form,
+    translateY: [20, 0],
+    opacity: [0, 1],
+  })
+;
