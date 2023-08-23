9447b85dbab544543dc674b4db505c2fe16977f0
Lucasgrimb <108102466+Lucasgrimb@users.noreply.github.com>
2023-08-23 10:00:19 -0300

    ki

--- Dj's view/Hello page.js
+++ Dj's view/Hello page.js
@@ -2,13 +2,20 @@ const title = document.querySelector('.header-gradient');
 const description = document.querySelector('.color-gray');
 const form = document.querySelector('form');
 const button = document.querySelector('.login-cta');
-const nav = document.querySelector('.nav')
-const usuario = document.getElementById("usuario")
-/*
-usuario.addEventListener("click", async (e)=> {
-  const req = await fetch("https://")
-})
-*/
+const nav = document.querySelector('.nav');
+const usuario = document.getElementById("usuario");
+const usuerName = document.getElementById("text-input");
+const password = document.getElementById("tex1-input1");
+
+button.addEventListener("click",  ()=> {
+const userN = usuerName.value;
+const pass = password.value;
+fetch('localhost:3000/api/login')
+
+});
+
+
+
 // Animacion de entrada//
 const animation = anime.timeline({
   easing: 'easeOutExpo',
@@ -36,4 +43,4 @@ animation
     translateY: [20, 0],
     opacity: [0, 1],
   })
-;
+
