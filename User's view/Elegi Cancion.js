8c0cca5050875bea70a971a63e1d6b58af86af25
FrancoFelstinsky <francofelstinsky@gmail.com>
2023-08-24 00:46:47 -0300

    no sirve este

--- User's view/Elegi Cancion.js
+++ User's view/Elegi Cancion.js
@@ -7,91 +7,28 @@
 */
 
 
-
 // Función para obtener el token de acceso de Spotify
 function requestSpotifyToken() {
-    // Configura tus credenciales de la aplicación Spotify
-    const clientId = '4ba679a5493041059789f92a2c776588'; 
-    const clientSecret = 'f03ee7bb97574f5fa9b1dab44d615c97'; 
-
-    // Endpoint de Spotify para obtener el token
-    const tokenEndpoint = 'https://accounts.spotify.com/api/token';
-    const data = 'grant_type=client_credentials';
-
-    // Codifica tus credenciales en Base64
-    const base64Credentials = btoa(clientId + ':' + clientSecret);
-
-    // Hace una petición a Spotify para obtener el token
-    return fetch(tokenEndpoint, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-            'Authorization': 'Basic ' + base64Credentials
-        },
-        body: data
-    })
-    .then(response => response.json())
-    .then(data => {
-        if (data.error) {
-            throw new Error(data.error_description);
-        }
-        return data.access_token;
-    });
+    // ... (código previo)
 }
 
 // Función para buscar canciones en Spotify con un token dado
 function searchTrack(query, token) {
-    // Hace una petición a Spotify para buscar canciones basadas en la query
-    return fetch(`https://api.spotify.com/v1/search?q=${query}&type=track&limit=10`, {
-        headers: {
-            'Authorization': 'Bearer ' + token
-        }
-    })
-    .then(response => response.json())
-    .then(data => {
-        if (data.error) {
-            throw new Error(data.error.message);
-        }
-        return data.tracks.items;
-    });
+    // ... (código previo)
 }
 
 // Función para mostrar los resultados de búsqueda en el DOM
 function displayResults(tracks) {
-    // Obtiene el contenedor de resultados del DOM
     const resultsDiv = document.getElementById('resultsDropdown'); 
     resultsDiv.innerHTML = '';
 
-    // Para cada pista obtenida, crea elementos del DOM para mostrarla
     tracks.forEach((track) => {
-        const image = track.album.images[0].url;
-        const title = track.name;
-        const artist = track.artists[0].name;
-        const trackId = track.id;
+        // ... (código previo)
 
         const resultDiv = document.createElement('div');
-        resultDiv.classList.add('result');
+        resultDiv.classList.add('result', 'song-result'); // Agrega clases aquí
 
-        const imageElement = document.createElement('img');
-        imageElement.src = image;
-        imageElement.alt = title;
-
-        const titleElement = document.createElement('p');
-        titleElement.textContent = title;
-
-        const artistElement = document.createElement('p');
-        artistElement.textContent = artist;
-
-        const trackIdElement = document.createElement('p');
-        trackIdElement.textContent = "ID: " + trackId;
-        trackIdElement.classList.add('track-id');
-
-        resultDiv.appendChild(imageElement);
-        resultDiv.appendChild(titleElement);
-        resultDiv.appendChild(artistElement);
-
-        
-       // resultDiv.appendChild(trackIdElement);
+        // ... (código previo)
 
         resultsDiv.appendChild(resultDiv);
         resultsDiv.style.display = 'block';  // Muestra el contenedor de resultados
@@ -99,38 +36,19 @@ function displayResults(tracks) {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-
-    // Manejador de evento para el botón de búsqueda
-    document.querySelector('.searchbtn').addEventListener('click', (e) => {
-        e.preventDefault();
-        performSearch();
-    });
-
-    // Manejador de evento para el campo de búsqueda
-    document.querySelector('#search').addEventListener('input', (e) => {
-        performSearch();
-    });
+    // ... (código previo)
 
     function performSearch() {
-        const query = document.getElementById('search').value;
-        if (!query) {
-            document.getElementById('resultsDropdown').style.display = 'none';
-            return;
-        }
-
-        requestSpotifyToken()
-            .then(token => {
-                return searchTrack(query, token);
-            })
-            .then(tracks => {
-                displayResults(tracks);
-            })
-            .catch(error => {
-                console.error('Error:', error.message);
-            });
+        // ... (código previo)
     }
 
-    // ... Aquí irían tus funciones requestSpotifyToken, searchTrack, y displayResults, sin cambios.
-
+    // Manejador de evento para hacer clic en un resultado de canción
+    document.getElementById('resultsDropdown').addEventListener('click', (e) => {
+        if (e.target.classList.contains('song-result')) {
+            // Acción que se ejecutará al hacer clic en una canción
+            // Por ejemplo, mostrar los nuevos botones y ocultar el resto de la página
+            // ... (agrega tu lógica aquí)
+        }
+    });
 });
 
