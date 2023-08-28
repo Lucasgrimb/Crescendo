523ebf25210ff48c2eec3ae2b1dda1120c3ad008
AbruRozenblum <110607863+AbruRozenblum@users.noreply.github.com>
2023-08-28 15:01:11 -0300

    v

--- Dj's view/Hello page.js
+++ Dj's view/Hello page.js
@@ -41,40 +41,45 @@ animation
 
 // main.js
 
-
 button.addEventListener('click', async function () {
+    event.preventDefault();  // Prevenimos la recarga automática del formulario
 
-  const loginData = {
-      userName: userName,
-      password: password
-  };
+    const userName = ""; 
+    const password = ""; 
 
-  console.log(loginData)
+    // Crear el objeto loginData con los valores
+    const loginData = {
+        userName: userName,
+        password: password
+    };
 
-  // try {
+    // Mostrar el objeto loginData en la consola
+    console.log(loginData);
 
-  //     const response = await fetch('http://localhost:3000/api/login', {
-  //         method: 'POST',
-  //         headers: {
-  //             'Content-Type': 'application/json'
-  //         },
-  //         body: JSON.stringify(loginData)
-  //     });
+    try {
+        const response = await fetch('http://localhost:3000/api/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(loginData)
+        });
 
-  //     const responseData = await response.json();
-  //     console.log(responseData);  // Log the response data
+        const responseData = await response.json();
+        console.log(responseData);  // Log the response data
 
-  //     if (responseData.ok) {
-  //         // Successful login
-  //         document.getElementById('message').textContent = 'Login successful!';
-  //     } else {
-  //         // Failed login
-  //         document.getElementById('message').textContent = 'Login failed. Please check your credentials.';
-  //     }
-  // } catch (error) {
-  //     console.error('Error during login:', error);
-  // }
+        if (responseData.ok) {
+            // Successful login
+            document.getElementById('message').textContent = 'Login successful!';
+        } else {
+            // Failed login
+            document.getElementById('message').textContent = 'Login failed. Please check your credentials.';
+        }
+    } catch (error) {
+        console.error('Error during login:', error);
+    }
 });
+
 
 
 
