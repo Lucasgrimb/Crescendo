62d87f7d404d1a2372dddf49b68cd08dc1f36565
Lucasgrimb <108102466+Lucasgrimb@users.noreply.github.com>
2023-08-30 15:15:49 -0300

    j

--- Dj's view/Register.js
+++ Dj's view/Register.js
@@ -1,23 +1,29 @@
 function registerUser() {
+    const email = document.getElementById("email").value;
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
     // Validar que los campos no estén vacíos
-    if (username === "" || password === "") {
+    if (email === "" || username === "" || password === "") {
         alert("Por favor, complete todos los campos.");
         return;
     }
 
     // Mostrar el mensaje de registro exitoso en una alerta
     const userData = {
+        email: email,
         username: username,
         password: password,
     };
 
     const successMessage = document.getElementById("successMessage");
-    successMessage.innerText = `¡Registro exitoso!\n\nNombre de usuario: ${userData.username}\nContraseña: ${userData.password}`;
+    successMessage.innerText = `¡Registro exitoso!\n\nCorreo electrónico: ${userData.email}\nNombre de usuario: ${userData.username}\nContraseña: ${userData.password}`;
     successMessage.style.display = "block";
 
-    // // Limpiar los campos después de mostrar el mensajeg
-    // document.getElementById("username").value = "";
-    // document.getElementById("password").value = "";
+    // Limpiar los campos después de mostrar el mensaje
+    document.getElementById("email").value = "";
+    document.getElementById("username").value = "";
+    document.getElementById("password").value = "";
 
     // Redirigir a la página "success.html"
     window.location.href = "Start Party.html";
@@ -34,7 +40,7 @@ registerForm.addEventListener('submit', async function (event) {
     const password = document.getElementById('password').value;
 
     const userData = {
-        userName: username,
+        username: username,
         password: password,
     };
 
@@ -60,9 +66,7 @@ registerForm.addEventListener('submit', async function (event) {
             // Failed login
             document.getElementById('message').textContent = 'Register failed. Please check your credentials.';
         }
-
     } catch (error) {
         console.error('Error during login:', error);
     }
 });
-
