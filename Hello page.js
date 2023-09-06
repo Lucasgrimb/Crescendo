fbac28efbeba088fa5d4384c7bf291c0e7f69b18
Lucasgrimb <108102466+Lucasgrimb@users.noreply.github.com>
2023-09-05 22:09:53 -0300

    j

--- Hello page.js
+++ Hello page.js
@@ -3,18 +3,12 @@ const description = document.querySelector('.color-gray');
 const form = document.querySelector('form');
 const button = document.querySelector('.login-cta');
 const nav = document.querySelector('.nav');
-var usernameValue = document.getElementById('username').value;
-var passwordValue = document.getElementById('password').value;
 
-
-
-
-// Animacion de entrada//
+// Animación de entrada
 const animation = anime.timeline({
   easing: 'easeOutExpo',
   duration: 500,
 });
-
 
 animation
   .add({
@@ -36,61 +30,54 @@ animation
     targets: form,
     translateY: [20, 0],
     opacity: [0, 1],
-  })
+  });
 
-// main.js
+button.addEventListener('click', async function (event) {
+  event.preventDefault(); // Prevenimos la recarga automática del formulario
 
-button.addEventListener('click', async function () {
-    event.preventDefault();  // Prevenimos la recarga automática del formulario
+  // Obtener los valores de los inputs
+  let usernameValue = document.getElementById('username').value;
+  let passwordValue = document.getElementById('password').value;
 
-    // Volver a poner los valores de los inputs ya que cambiaron de lo que eran inicialmente
-    usernameValue = document.getElementById('username').value;
-    passwordValue = document.getElementById('password').value;
-    // Crea el objeto loginData con los valores ingresados
-    const loginData = {
-      userName: usernameValue,
-      password: passwordValue
+  // Crea el objeto loginData con los valores ingresados
+  const loginData = {
+    userName: usernameValue,
+    password: passwordValue,
   };
 
-    // Mostrar el objeto loginData en la consola
-    console.log(loginData); 
+  // Mostrar el objeto loginData en la consola
+  console.log(loginData);
 
-    try {
-        const response = await fetch('https://crescendoapi-pro.vercel.app/api/login', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(loginData),
-            credentials: "include",
-        });
+  try {
+    const response = await fetch('https://crescendoapi-pro.vercel.app/api/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(loginData),
+      credentials: 'include',
+    });
 
-        const responseData = await response.json();
-        console.log(responseData);  // Log the response data
-        // Supongamos que `response` es la respuesta del servidor
-const tokens = response.data;
-
-// Almacenar tokens en localStorage
-localStorage.setItem('accessToken', tokens.accessToken);
-localStorage.setItem('refreshToken', tokens.refreshToken);
-
-        
-
-        if (responseData.message) {
-            // Successful login
-            document.getElementById('message').textContent = 'Login successful!';
-            setTimeout(function() {
-
-             window.location.href = "Qr code.html";
-          }, 700);  // 3000 milisegundos = 3 segundos
-
-        } else {
-            // Failed login
-            document.getElementById('message').textContent = 'Inicio de sesion fallido. Porfavor chequeee su informacion';
-        }
-    } catch (error) {
-        console.error('Error during login:', error);
+    if (!response.ok) {
+      throw new Error(`Failed to login: ${response.status}`);
     }
+
+    const responseData = await response.json();
+    console.log(responseData); // Log the response data
+
+    // Almacenar tokens en localStorage
+    localStorage.setItem('accessToken', responseData.accessToken);
+    localStorage.setItem('refreshToken', responseData.refreshToken);
+
+    // Login exitoso
+    document.getElementById('message').textContent = 'Login successful!';
+    setTimeout(function() {
+      window.location.href = "Qr code.html";
+    }, 700); // 700 milisegundos = 0.7 segundos
+
+  } catch (error) {
+    console.log(`Error during login: ${error}`);
+    // Login fallido
+    document.getElementById('message').textContent = 'Inicio de sesion fallido. Porfavor chequeee su informacion';
+  }
 });
-
-
