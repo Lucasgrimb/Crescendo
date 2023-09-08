8a08424e576021b6787eee79783390b8a6e8b261
AbruRozenblum <110607863+AbruRozenblum@users.noreply.github.com>
2023-09-08 08:55:17 -0300

    cambios btn guardar qr

--- Scan.js
+++ Scan.js
@@ -107,27 +107,21 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 //btn guardar qr
-import html2canvas from 'html2canvas';
-const html2canvas = require('html2canvas');
-html2canvas(element).then(function(canvas) {
+// Obtén una referencia al botón "Guardar" y la imagen del código QR
+var guardarButton = document.getElementById("guardarButton");
+var qrImage = document.getElementById("qrImage");
 
-document.addEventListener('DOMContentLoaded', () => {
-  const guardarButton = document.getElementById('guardarButton');
-  
-  guardarButton.addEventListener('click', () => {
-    // Selecciona el elemento que contiene el código QR que deseas guardar
-    const qrContainer = document.getElementById('codigoQR');
-    
-    // Usa html2canvas para capturar el contenido como una imagen
-    html2canvas(qrContainer).then((canvas) => {
-      // Crea un enlace para descargar la imagen
-      const link = document.createElement('a');
-      link.href = canvas.toDataURL('image/png'); // La imagen se convierte a formato PNG
-      link.download = 'codigo_qr.png'; // Nombre del archivo a descargar
-      
-      // Simula un clic en el enlace para iniciar la descarga
-      link.click();
-      });
-    });
-  });
+// Agrega un evento clic al botón "Guardar"
+guardarButton.addEventListener("click", function () {
+    // Crea un enlace temporal
+    var link = document.createElement("a");
+    link.href = qrImage.src;
+    link.download = "codigo_qr.png"; // Nombre del archivo a descargar
+
+    // Simula un clic en el enlace para iniciar la descarga
+    document.body.appendChild(link);
+    link.click();
+
+    // Limpia el enlace temporal
+    document.body.removeChild(link);
 });
