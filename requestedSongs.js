d8f0fdc00a27ce1e9612c099033d3f29175011cc
Lucasgrimb <108102466+Lucasgrimb@users.noreply.github.com>
2023-09-08 23:52:53 -0300

    kk

--- requestedSongs.js
+++ requestedSongs.js
@@ -1,40 +1,48 @@
-// Supongamos que has guardado los IDs de las canciones en un array bajo la clave 'songIds' y el party_id bajo la clave 'partyId' en localStorage
-const storedSongIds = localStorage.getItem('songIds'); // Esto devolverá una cadena JSON
+// Supongamos que has guardado el party_id bajo la clave 'partyId' en localStorage
 const storedPartyId = localStorage.getItem('partyId');
 
-if (!storedSongIds || !storedPartyId) {
-  console.log("No hay datos en el localStorage");
+if (!storedPartyId) {
+  console.log("No hay partyId en el localStorage");
 } else {
-  const songIds = JSON.parse(storedSongIds); // Convierte la cadena JSON en un array
   const party_id = storedPartyId; // Ya es una cadena, no necesita ser parseado
+  
+  // Obteniendo los songIds asociados al party_id desde localStorage
+  const storedSongIds = localStorage.getItem(party_id); // Esto devolverá una cadena JSON
 
-  // Datos a enviar en el cuerpo de la solicitud POST
-  const data = {
-    songIds,
-    party_id
-  };
+  if (!storedSongIds) {
+    console.log("No hay songIds en el localStorage para este party_id");
+  } else {
+    const songIds = JSON.parse(storedSongIds); // Convierte la cadena JSON en un array
 
-  // Opciones de la solicitud fetch
-  const requestOptions = {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(data)
-  };
+    // Datos a enviar en el cuerpo de la solicitud POST
+    const data = {
+      songIds,
+      party_id
+    };
 
-  // Realizar la solicitud al endpoint
-  fetch('https://crescendoapi-pro.vercel.app/api/get-multiple-songs', requestOptions)
-    .then(response => response.json())
-    .then(songs => {
-      // Mostrar la información en la consola
-      songs.forEach(song => {
-        console.log(`Foto: ${song.image}`);
-        console.log(`Nombre: ${song.name}`);
-        console.log(`Artista: ${song.artist}`);
-        console.log(`Estado: ${song.songState}`);
-        console.log('---');
-      });
-    })
-    .catch(error => console.error('Error:', error));
+    // Opciones de la solicitud fetch
+    const requestOptions = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data)
+    };
+
+    // Realizar la solicitud al endpoint
+    fetch('https://crescendoapi-pro.vercel.app/api/get-multiple-songs', requestOptions)
+      .then(response => response.json())
+      .then(songs => {
+        // Mostrar la información en la consola
+        songs.forEach(song => {
+          console.log(`Foto: ${song.image}`);
+          console.log(`Nombre: ${song.name}`);
+          console.log(`Artista: ${song.artist}`);
+          console.log(`Estado: ${song.songState}`);
+          console.log('---');
+        });
+      })
+      .catch(error => console.error('Error:', error));
+  }
 }
+
