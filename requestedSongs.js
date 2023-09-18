8975896806924b218fa247a2363c06d5e860db2e
Lucasgrimb <108102466+Lucasgrimb@users.noreply.github.com>
2023-09-18 11:29:47 -0300

    k

--- requestedSongs.js
+++ requestedSongs.js
@@ -1,94 +1,88 @@
-// Obtener el parámetro 'party_id' de la URL
-const params = new URLSearchParams(window.location.search);
-const partyId = params.get('party_id');
+document.addEventListener('DOMContentLoaded', function() {
+  // Obtener el parámetro 'party_id' de la URL
+  const params = new URLSearchParams(window.location.search);
+  const partyId = params.get('party_id');
 
-// Función para obtener las canciones seleccionadas
-async function getSelectedSongs(party_id) {
-  try {
-    const response = await fetch(`https://crescendoapi-pro.vercel.app/api/selectedsongs/${party_id}`, {
-      method: "GET",
-      headers: {
-       // "Authorization": `Bearer ${accessToken}`,
-      },
-    });
+  // Llama a la función para obtener las canciones seleccionadas
+  getSelectedSongs(partyId);
 
-    if (response.status === 403) {
-      // Intenta obtener un nuevo accessToken si el anterior fue rechazado
-      const newAccessToken = await fetchAccessToken();
-      if (newAccessToken) {
-        return getSelectedSongs(party_id, newAccessToken);
+  // Función para obtener las canciones seleccionadas
+  async function getSelectedSongs(party_id) {
+      try {
+          const response = await fetch(`https://crescendoapi-pro.vercel.app/api/selectedsongs/${party_id}`, {
+              method: "GET",
+              headers: {
+
+              },
+          });
+
+          if (!response.ok) {
+              throw new Error("No se pudo obtener las canciones seleccionadas");
+          }
+
+          const data = await response.json();
+          console.log(data);
+
+          // Haz algo con los datos, como mostrar las canciones
+          displaySongs(data);
+      } catch (error) {
+          console.error(error);
       }
-    }
-
-    if (!response.ok) {
-      throw new Error("No se pudo obtener las canciones seleccionadas");
-    }
-
-    const data = await response.json();
-console.log(data);
-    // Haz algo con los datos, como mostrar las canciones
-    displaySongs(data);
-  } catch (error) {
-    console.error(error);
   }
-}
 
+  // Función para mostrar las canciones
+  function displaySongs(songs) {
+      const songContainer = document.getElementById('song-container');
+      const acceptedContainer = document.querySelector('.song-container-accepted');
+      const rejectedContainer = document.querySelector('.song-container-rejected')
 
+      songContainer.innerHTML = "";
+      acceptedContainer.innerHTML = ""; // Limpiar el contenedor de canciones aceptadas
+      rejectedContainer.innerHTML = "";
 
-function displaySongs(songs) {
-  const songContainer = document.getElementById('song-container');
-  const acceptedContainer = document.querySelector('.song-container-accepted'); 
-  const rejectedContainer = document.querySelector('.song-container-rejected')
+      if (songs.length === 0) {
+          const noSongsMessage = document.createElement('div');
+          noSongsMessage.className = "no-songs";
+          noSongsMessage.innerText = "No hay canciones para mostrar";
+          songContainer.appendChild(noSongsMessage);
+      } else {
+          songs.forEach((song) => {
+              const songItem = document.createElement('div');
+              songItem.className = "song-item";
+              songItem.setAttribute('data-songid', song.id);
+              songItem.setAttribute('data-songstate', song.song_state);
 
-  songContainer.innerHTML = "";
-  acceptedContainer.innerHTML = ""; // Clear the accepted songs container
-  rejectedContainer.innerHTML = "";
+              const songImage = document.createElement('div');
+              songImage.className = "song-image";
+              const imgElement = document.createElement("img");
+              imgElement.src = song.image;
+              songImage.appendChild(imgElement);
 
-  if (songs.length === 0) {
-    const noSongsMessage = document.createElement('div');
-    noSongsMessage.className = "no-songs";
-    noSongsMessage.innerText = "No hay canciones para mostrar";
-    songContainer.appendChild(noSongsMessage);
-  } else {
-    songs.forEach((song) => {
-      const songItem = document.createElement('div');
-      songItem.className = "song-item";
-      songItem.setAttribute('data-songid', song.id);
-      songItem.setAttribute('data-songstate', song.song_state);
-    
-          const songImage = document.createElement('div');
-          songImage.className = "song-image";
-          const imgElement = document.createElement("img");
-          imgElement.src = song.image;
-          songImage.appendChild(imgElement);
-    
-          const songDetails = document.createElement('div');
-          songDetails.className = "song-details";
-          const songTitle = document.createElement('p');
-          songTitle.className = "song-title";
-          songTitle.innerText = song.name;
-          const songArtist = document.createElement('p');
-          songArtist.className = "song-artist";
-          songArtist.innerText = song.artist;
-          
+              const songDetails = document.createElement('div');
+              songDetails.className = "song-details";
+              const songTitle = document.createElement('p');
+              songTitle.className = "song-title";
+              songTitle.innerText = song.name;
+              const songArtist = document.createElement('p');
+              songArtist.className = "song-artist";
+              songArtist.innerText = song.artist;
 
-      
-          songDetails.appendChild(songTitle);
-          songDetails.appendChild(songArtist);
-          songItem.appendChild(songImage);
-          songItem.appendChild(songDetails);
+              songDetails.appendChild(songTitle);
+              songDetails.appendChild(songArtist);
+              songItem.appendChild(songImage);
+              songItem.appendChild(songDetails);
 
-   // Decide dónde añadir el songItem en función de su estado
-   if (songItem.getAttribute('data-songstate') === 'accepted') {
-    acceptedContainer.appendChild(songItem);
-    songItem.classList.add("accepted");
-  } else if (songItem.getAttribute('data-songstate') === 'rejected') {
-    rejectedContainer.appendChild(songItem);
-    songItem.classList.add("rejected");
-
-  } else {
-    songContainer.appendChild(songItem);
+              // Decide dónde añadir el songItem en función de su estado
+              if (songItem.getAttribute('data-songstate') === 'accepted') {
+                  acceptedContainer.appendChild(songItem);
+                  songItem.classList.add("accepted");
+              } else if (songItem.getAttribute('data-songstate') === 'rejected') {
+                  rejectedContainer.appendChild(songItem);
+                  songItem.classList.add("rejected");
+              } else {
+                  songContainer.appendChild(songItem);
+              }
+          });
+      }
   }
 });
-}
-}
