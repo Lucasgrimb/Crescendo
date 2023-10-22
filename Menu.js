d9dd977070384289f021bd9a21e6ee8a3e0e6ee8
FrancoFelstinsky <francofelstinsky@gmail.com>
2023-10-22 19:14:39 -0300

    made effect

--- Menu.js
+++ Menu.js
@@ -12,3 +12,40 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   
 });
+
+
+let valueDisplays = document.querySelectorAll(".num");
+let interval = 2000; // Cambia el intervalo a 2 segundos (2000 ms)
+
+// Obtén la referencia a la sección "Portfolio"
+let section = document.querySelector(".Portfolio");
+
+// Agrega una variable para rastrear si la animación ya ha comenzado
+let started = false;
+
+window.onscroll = function () {
+    if (window.scrollY >= section.offsetTop) {
+        if (!started) {
+            valueDisplays.forEach((valueDisplay) => startCount(valueDisplay));
+        }
+        started = true;
+    }
+};
+
+function startCount(valueDisplay) {
+    let startValue = 0;
+    let endValue = parseInt(valueDisplay.getAttribute("data-val"));
+    let duration = Math.floor(interval / endValue);
+    let counter = setInterval(function () {
+        startValue += 1;
+        valueDisplay.textContent = "+" + startValue; // Agrega el símbolo "+" al número
+        if (startValue == endValue) {
+            clearInterval(counter);
+        }
+    }, duration);
+}
+
+
+
+
+
