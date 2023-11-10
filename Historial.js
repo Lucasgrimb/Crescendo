e2e08d6203a77040c8721392e73e870ab703f560
Lucasgrimb <108102466+Lucasgrimb@users.noreply.github.com>
2023-11-09 22:43:07 -0300

    k

--- Historial.js
+++ Historial.js
@@ -2,42 +2,39 @@ document.addEventListener('DOMContentLoaded', function () {
     const createPartyBtn = document.getElementById('createPartyBtn');
     const partiesList = document.getElementById('partiesList');
 
-    createPartyBtn.addEventListener('click', displayCreatePartyForm);
+    // Evento para manejar la creación de una nueva fiesta
+    createPartyBtn.addEventListener('click', handleCreateParty);
 
-    function displayCreatePartyForm() {
-        const formHtml = `
-            <form id="createPartyForm">
-                <input type="text" id="partyName" placeholder="Nombre de la fiesta" required />
-                <button type="submit">Crear Fiesta</button>
-            </form>
-        `;
-        partiesList.innerHTML = formHtml;
-
-        document.getElementById('createPartyForm').addEventListener('submit', handleCreateParty);
-    }
-
+    // Manejador del evento para crear una nueva fiesta
     function handleCreateParty(event) {
-        event.preventDefault();
-        const partyName = document.getElementById('partyName').value;
+        event.preventDefault(); // Prevenir la recarga por defecto del formulario, si es que existe
         fetch('https://crescendoapi-pro.vercel.app/api/createParty', {
             method: 'POST',
             headers: {
                 'Authorization': `Bearer ${localStorage.getItem('accessToken')}`,
                 'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ partyName })
+            }
+            // No es necesario enviar body ya que el nombre de la fiesta se generará en el servidor
         })
-        .then(response => response.json())
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            return response.json();
+        })
         .then(data => {
             if (data.success) {
-                loadUserParties(); // Recargar la lista de fiestas
+                loadUserParties(); // Recargar la lista de fiestas después de crear una nueva fiesta
             } else {
                 console.error(data.error);
             }
         })
-        .catch(error => console.error('Error al crear la fiesta:', error));
+        .catch(error => {
+            console.error('Error al crear la fiesta:', error);
+        });
     }
 
+    // Función para cargar las fiestas del usuario
     function loadUserParties() {
         fetch('https://crescendoapi-pro.vercel.app/api/getDjHistory', {
             headers: {
@@ -52,21 +49,29 @@ document.addEventListener('DOMContentLoaded', function () {
                 console.error(data.error);
             }
         })
-        .catch(error => console.error('Error al obtener fiestas:', error));
-    }
-
-    function renderPartiesList(parties) {
-        partiesList.innerHTML = ''; // Limpiar la lista actual
-
-        parties.forEach(party => {
-            const partyElement = document.createElement('div');
-            partyElement.className = 'party';
-            partyElement.innerHTML = `<h2>${party.party_name} - ${party.party_date}</h2>`;
-            partyElement.addEventListener('click', () => renderSongsList(party.songs));
-            partiesList.appendChild(partyElement);
+        .catch(error => {
+            console.error('Error al obtener fiestas:', error);
         });
     }
 
+    // Función para mostrar la lista de fiestas
+    function renderPartiesList(parties) {
+        partiesList.innerHTML = ''; // Limpiar la lista actual
+
+        if (parties.length === 0) {
+            partiesList.innerHTML = '<p>No hay fiestas para mostrar. ¡Crea tu primera fiesta!</p>';
+        } else {
+            parties.forEach(party => {
+                const partyElement = document.createElement('div');
+                partyElement.className = 'party';
+                partyElement.innerHTML = `<h2>${party.party_name} - ${party.party_date}</h2>`;
+                partyElement.addEventListener('click', () => renderSongsList(party.songs));
+                partiesList.appendChild(partyElement);
+            });
+        }
+    }
+
+    // Función para mostrar la lista de canciones de una fiesta seleccionada
     function renderSongsList(songs) {
         partiesList.innerHTML = ''; // Limpiar la lista actual
 
@@ -75,14 +80,19 @@ document.addEventListener('DOMContentLoaded', function () {
         backButton.addEventListener('click', loadUserParties);
         partiesList.appendChild(backButton);
 
-        const songsList = document.createElement('ul');
-        songs.forEach(song => {
-            const songItem = createSongElement(song);
-            songsList.appendChild(songItem);
-        });
-        partiesList.appendChild(songsList);
+        if (songs.length === 0) {
+            partiesList.innerHTML += '<p>No hay canciones para mostrar en esta fiesta.</p>';
+        } else {
+            const songsList = document.createElement('ul');
+            songs.forEach(song => {
+                const songItem = createSongElement(song);
+                songsList.appendChild(songItem);
+            });
+            partiesList.appendChild(songsList);
+        }
     }
 
+    // Función para crear el elemento HTML para una canción
     function createSongElement(song) {
         const songItem = document.createElement('li');
         songItem.className = `song-item ${song.song_state}`;
