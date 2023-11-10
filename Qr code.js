b74bb80895406eab0108b2d87341caf17646a50d
Lucasgrimb <108102466+Lucasgrimb@users.noreply.github.com>
2023-11-10 11:27:31 -0300

    m

--- Qr code.js
+++ Qr code.js
@@ -1,4 +1,6 @@
-var party_id;
+// Obtener el party_id de la URL
+const urlParams = new URLSearchParams(window.location.search);
+const party_id = urlParams.get('party_id');
 
 // Función para llamar al endpoint /api/token y obtener el accessToken
 async function fetchAccessToken() {
@@ -27,46 +29,15 @@ async function fetchAccessToken() {
   }
 }
 
-// Función para iniciar la fiesta
-async function startParty(accessToken) {
-  try {
-    const response = await fetch('https://crescendoapi-pro.vercel.app/api/startparty', {
-      method: "GET",
-      headers: {
-        "Authorization": `Bearer ${accessToken}`,
-      },
-    });
-
-    if (response.status === 403) {
-      const newAccessToken = await fetchAccessToken();
-      if (newAccessToken) {
-        return startParty(newAccessToken);
-      }
-    }
-
-    if (!response.ok) {
-      throw new Error("No se pudo iniciar la fiesta");
-    }
-
-    const data = await response.json();
-    party_id = data.party_id;
-
-
-    return data;
-  } catch (error) {
-    console.error(error);
-    return null;
-  }
-}
-
 // Función para obtener las canciones seleccionadas
-async function getSelectedSongs(party_id) {
+async function getSelectedSongs() {
   try {
+    const accessToken = await fetchAccessToken();
     const response = await fetch(`https://crescendoapi-pro.vercel.app/api/selectedsongs/${party_id}`, {
       method: "GET",
       headers: {
-
-      },
+        "Authorization": `Bearer ${accessToken}`,
+      }
     });
 
     if (!response.ok) {
@@ -74,19 +45,17 @@ async function getSelectedSongs(party_id) {
     }
 
     const data = await response.json();
-    console.log(data);
-    // Haz algo con los datos, como mostrar las canciones
-    displaySongs(data);
+    displaySongs(data.songs);
   } catch (error) {
     console.error(error);
   }
 }
 
-
-async function updateSongState(songId, party_id, action) {
+// Función para actualizar el estado de una canción
+async function updateSongState(songId, action) {
   try {
     const accessToken = await fetchAccessToken();
-    const response = await fetch(`https://crescendoapi-pro.vercel.app/api/${songId}/${party_id}/${action}`, {
+    const response = await fetch(`https://crescendoapi-pro.vercel.app/api/songs/${songId}/${party_id}/${action}`, {
       method: 'POST',
       headers: {
         'Authorization': `Bearer ${accessToken}`,
@@ -103,15 +72,15 @@ async function updateSongState(songId, party_id, action) {
     return false;
   }
 }
-// ...
 
+// Función para mostrar las canciones
 function displaySongs(songs) {
   const songContainer = document.getElementById('song-container');
   const acceptedContainer = document.querySelector('.song-container-accepted');
-  const rejectedContainer = document.querySelector('.song-container-rejected')
+  const rejectedContainer = document.querySelector('.song-container-rejected');
 
   songContainer.innerHTML = "";
-  acceptedContainer.innerHTML = ""; // Clear the accepted songs container
+  acceptedContainer.innerHTML = "";
   rejectedContainer.innerHTML = "";
 
   if (songs.length === 0) {
@@ -141,41 +110,18 @@ function displaySongs(songs) {
       songArtist.className = "song-artist";
       songArtist.innerText = song.artist;
 
-
       const acceptButton = document.createElement('button');
       acceptButton.className = "accept-song";
       acceptButton.innerText = "✓";
+      acceptButton.addEventListener('click', async () => {
+        // Implementación del botón de aceptar
+      });
 
       const rejectButton = document.createElement('button');
       rejectButton.className = "reject-song";
       rejectButton.innerText = "X";
-
-      // Agregar los manejadores de eventos aquí
-      acceptButton.addEventListener('click', async () => {
-        const songId = songItem.getAttribute('data-songid');
-        if (await updateSongState(songId, party_id, 'accept')) {
-          // Mueve la canción al final de la lista de canciones aceptadas
-          acceptedContainer.appendChild(songItem);
-          songItem.classList.add("accepted");
-          acceptButton.remove();
-          rejectButton.remove();
-        } else {
-          console.error(`Failed to accept song with ID: ${songId}`);
-        }
-      });
-
-
       rejectButton.addEventListener('click', async () => {
-        const songId = songItem.getAttribute('data-songid');
-        if (await updateSongState(songId, party_id, 'reject')) {
-          // Mueve la canción al final de la lista de canciones rechazadas
-          rejectedContainer.appendChild(songItem);
-          songItem.classList.add("rejected");
-          acceptButton.remove();
-          rejectButton.remove();
-        } else {
-          console.error(`Failed to reject song with ID: ${songId}`);
-        }
+        // Implementación del botón de rechazar
       });
 
       songDetails.appendChild(songTitle);
@@ -186,16 +132,10 @@ function displaySongs(songs) {
       songItem.appendChild(rejectButton);
 
       // Decide dónde añadir el songItem en función de su estado
-      if (songItem.getAttribute('data-songstate') === 'accepted') {
+      if (song.song_state === 'accepted') {
         acceptedContainer.appendChild(songItem);
-        songItem.classList.add("accepted");
-        acceptButton.remove();
-        rejectButton.remove();
-      } else if (songItem.getAttribute('data-songstate') === 'rejected') {
+      } else if (song.song_state === 'rejected') {
         rejectedContainer.appendChild(songItem);
-        songItem.classList.add("rejected");
-        acceptButton.remove();
-        rejectButton.remove();
       } else {
         songContainer.appendChild(songItem);
       }
@@ -203,68 +143,43 @@ function displaySongs(songs) {
   }
 }
 
-
-
-const songContainer = document.getElementById('song-container');
-songContainer.addEventListener('scroll', function () {
-  const containerHeight = songContainer.clientHeight;
-  const scrollHeight = songContainer.scrollHeight;
-  const scrollPosition = songContainer.scrollTop;
-
-  if (containerHeight + scrollPosition >= scrollHeight) {
-    // Aplica el estilo especial a la última canción
-    const lastSong = document.querySelector('.song-item.accepted:last-child');
-    if (lastSong) {
-      lastSong.classList.add('special-last-song');
-    }
-  } else {
-    // Elimina el estilo especial si no estamos en la última canción
-    const specialLastSong = document.querySelector('.song-item.accepted.special-last-song');
-    if (specialLastSong) {
-      specialLastSong.classList.remove('special-last-song');
-    }
-  }
-});
-
-async function main() {
-  console.log("MAIN")
-  const accessToken = await fetchAccessToken();
-  if (accessToken) {
-    // Llama a displaySongs con un arreglo vacío al inicio
-    displaySongs([]);
-    const partyData = await startParty(accessToken);
-    if (partyData && party_id) {
-      await getSelectedSongs(party_id, accessToken);
-    } else {
-      console.log("No hay datos de la fiesta o party_id");
-    }
-  }
-  }
-
-// Ejecuta la función main cuando se carga la página
-function actualizarPedidos() {
-  console.log("A")
-  $.ajax({
-    url: `https://crescendoapi-pro.vercel.app/api/selectedsongs/${party_id}`,
-    method: 'GET',
-    dataType: 'json',
-    success: function (data) {
-      // Update the section of requests with the new data
-      $('#song-container').html(data.pedidos);
-    },
-    error: function (error) {
-      console.error('Error loading song requests: ' + error);
-    }
-  });
+// Función para generar y mostrar el código QR
+async function generateQRCode() {
+    const url = `https://crescendo-nine.vercel.app/Elegi%20Cancion.html?party_id=${party_id}`;
+    const qr = await QRCode.toDataURL(url);
+    const qrContainer = document.getElementById('qr-container');
+    qrContainer.src = qr;
 }
 
-var intervalo = setInterval(actualizarPedidos, 1000);
-  console.log(intervalo)
+// Función principal que inicia las operaciones
+async function main() {
+    console.log("MAIN");
+    const accessToken = await fetchAccessToken();
+    if (accessToken) {
+        displaySongs([]);
+        await getSelectedSongs();
+    }
+}
 
+// Función para actualizar pedidos de canciones
+function actualizarPedidos() {
+    $.ajax({
+        url: `https://crescendoapi-pro.vercel.app/api/selectedsongs/${party_id}`,
+        method: 'GET',
+        dataType: 'json',
+        success: function (data) {
+            $('#song-container').html(data.pedidos);
+        },
+        error: function (error) {
+            console.error('Error loading song requests: ' + error);
+        }
+    });
+}
 
-// Set up the interval to update the requests every 5 seconds
+var intervalo = setInterval(actualizarPedidos, 5000);
 
-// To stop automatic updating, you can use clearInterval at some point
-// clearInterval(intervalo);
-
-window.addEventListener("load", main);
+// Evento para cargar las funciones principales cuando la página se carga
+window.addEventListener("load", () => {
+    main();
+    generateQRCode();
+});
