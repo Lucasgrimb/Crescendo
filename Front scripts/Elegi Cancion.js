9334a639d7040c2ed9cd1b41dc1941111be3ed58
Lucasgrimb <108102466+Lucasgrimb@users.noreply.github.com>
2023-11-24 18:51:32 -0300

    k

--- Front scripts/Elegi Cancion.js
+++ Front scripts/Elegi Cancion.js
@@ -125,13 +125,20 @@ async function handleAcceptClick() {
 
     try {
         await sendRequestWithRetry('https://energetic-gown-elk.cyclic.app/api/store-song-request', requestOptions, 3);
-        // Recarga la página después de recibir una respuesta exitosa
-        location.reload();
+        // Mostrar el modal de confirmación en lugar de recargar directamente
+        document.getElementById('confirmationModal').style.display = 'flex';
     } catch (error) {
         console.error('Error:', error);
         alert('Hubo un problema al guardar la solicitud de la canción. Intente nuevamente.');
     }
 }
+
+// Agrega el manejador para el botón de cierre del modal
+document.getElementById('closeModal').addEventListener('click', () => {
+    document.getElementById('confirmationModal').style.display = 'none';
+    location.reload(); // Recargar la página después de cerrar el modal
+});
+
 
 async function sendRequestWithRetry(url, options, retries) {
     for (let i = 0; i < retries; i++) {
