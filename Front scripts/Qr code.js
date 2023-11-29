a716be4619ff7895e40406c51dd3e83cb96b430e
FrancoFelstinsky <francofelstinsky@gmail.com>
2023-11-29 05:53:32 -0300

    -100

--- Front scripts/Qr code.js
+++ Front scripts/Qr code.js
@@ -10,8 +10,8 @@ function displaySongs(songs) {
 
   const peticionesHeight = songs.length * 68;
 
-  acceptedContainer.style.top = `${peticionesHeight + gapHeight - 30}px`;
-  rejectedContainer.style.top = `${peticionesHeight + gapHeight - 30}px`;
+  acceptedContainer.style.top = `${peticionesHeight + gapHeight - 100}px`;
+  rejectedContainer.style.top = `${peticionesHeight + gapHeight - 100}px`;
 
   songContainer.innerHTML = "";
   acceptedContainer.innerHTML = "";
