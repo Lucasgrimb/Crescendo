0288192c148d64eae7a1a98453c100ded82f8fbf
FrancoFelstinsky <francofelstinsky@gmail.com>
2023-12-01 12:02:14 -0300

    70again

--- Front scripts/Qr code.js
+++ Front scripts/Qr code.js
@@ -216,7 +216,7 @@ function ajustarPosiciones() {
   }, 0);
 
   // Define la altura adicional en píxeles debajo de la última canción
-  const alturaAdicional = 20; // Ajusta este valor según sea necesario
+  const alturaAdicional = 70; // Ajusta este valor según sea necesario
   const espacioEntreSecciones = 30; // Nuevo: Espacio entre secciones aceptadas y rechazadas
 
   // Ajusta la posición de los elementos .accept-peticion y .reject-peticion
