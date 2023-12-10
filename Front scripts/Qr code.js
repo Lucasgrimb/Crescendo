fd3fcc12ecd4ce58b85334601f247a726fc5865a
FrancoFelstinsky <francofelstinsky@gmail.com>
2023-12-09 22:47:25 -0300

    a

--- Front scripts/Qr code.js
+++ Front scripts/Qr code.js
@@ -296,7 +296,7 @@ function ajustarPosiciones() {
     rejectPeticion.style.top = "300px";  // Altura específica para rechazadas
   } else {
     // Caso normal con peticiones, ajusta en función de la altura total y el espacio entre secciones
-    acceptPeticion.style.top = `${peticionesHeight + alturaAdicional * 1.0}px`;
+    acceptPeticion.style.top = `${peticionesHeight + alturaAdicional * 0.5}px`;
     rejectPeticion.style.top = `${peticionesHeight + alturaAdicional * 3.0 + espacioEntreSecciones}px`;
   }
 }
