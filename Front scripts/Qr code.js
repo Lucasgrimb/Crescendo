75b7edc66d0c54c21d6bf93ef7c348d07315e77d
FrancoFelstinsky <francofelstinsky@gmail.com>
2024-02-08 02:31:52 -0300

    back to

--- Front scripts/Qr code.js
+++ Front scripts/Qr code.js
@@ -313,12 +313,10 @@ function actualizarUltimaCancion() {
 
 // Función principal que inicia las operaciones
 async function main() {
-  document.getElementById('loadingSpinner').style.display = 'block';
-  await getSelectedSongs(party_id);
-  document.getElementById('loadingSpinner').style.display = 'none';
-  document.getElementById('song-container').style.display = 'flex';
+    document.getElementById('loadingSpinner').style.display = 'block';
+    await getSelectedSongs(party_id);
+    document.getElementById('loadingSpinner').style.display = 'none';
 }
-
 
 window.addEventListener("load", () => {
     const qrLink = document.getElementById('qrLink');
