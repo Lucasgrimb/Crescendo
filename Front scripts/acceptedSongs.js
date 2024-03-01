d9d756d5a014a7325888a80e292a739d6e768d15
Lucasgrimb <108102466+Lucasgrimb@users.noreply.github.com>
2024-03-01 14:06:54 -0300

    k

--- Front scripts/acceptedSongs.js
+++ Front scripts/acceptedSongs.js
@@ -1,52 +1,85 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const partyId = getPartyIdFromUrl();
-    if(partyId) {
-        fetchAcceptedSongs(partyId);
-    } else {
-        console.error('No party ID found in the URL');
+const urlParams = new URLSearchParams(window.location.search);
+const party_id = urlParams.get('party_id');
+console.log(party_id);
+
+// Función para obtener el token de acceso
+async function fetchAccessToken() {
+    try {
+        const refreshToken = localStorage.getItem('refreshToken');
+        if (!refreshToken) {
+            throw new Error('No se encontró refreshToken en localStorage');
+        }
+
+        const response = await fetch('https://energetic-gown-elk.cyclic.app/api/token', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ refreshToken })
+        });
+
+        if (!response.ok) {
+            throw new Error('La petición de token no fue exitosa');
+        }
+
+        const data = await response.json();
+        localStorage.setItem('accessToken', data.accessToken);
+        localStorage.setItem('refreshToken', data.refreshToken);
+        return data.accessToken;
+    } catch (error) {
+        console.error('Hubo un problema con la petición de fetch:', error);
+        alert('Error al obtener el token de acceso. Por favor, intente nuevamente.');
+        return null;
     }
-});
-
-function getPartyIdFromUrl() {
-    const urlParams = new URLSearchParams(window.location.search);
-    return urlParams.get('party_id'); // Asume que el ID de la fiesta está en el URL como ?party_id=tu_party_id
 }
 
-function fetchAcceptedSongs(partyId) {
-    fetch(`https://energetic-gown-elk.cyclic.app/api/selectedsongs/${partyId}`)
-        .then(response => response.json())
-        .then(songs => {
-            const acceptedSongs = songs.filter(song => song.song_state === 'accepted');
-            const container = document.getElementById('songsContainer');
-            acceptedSongs.forEach(song => displaySong(song, container));
-        })
-        .catch(error => console.error('Error loading songs:', error));
+// Función para obtener las canciones aceptadas
+async function getSelectedSongs(party_id) {
+    try {
+        let accessToken = localStorage.getItem('accessToken');
+        if (!accessToken) {
+            accessToken = await fetchAccessToken();
+        }
+
+        const response = await fetch(`https://energetic-gown-elk.cyclic.app/api/selectedsongs/${party_id}`, {
+            method: "GET",
+            headers: {
+                'Authorization': `Bearer ${accessToken}`
+            }
+        });
+
+        if (!response.ok) {
+            throw new Error("No se pudo obtener las canciones seleccionadas");
+        }
+
+        const songs = await response.json();
+        const acceptedSongs = songs.filter(song => song.song_state === 'accepted');
+        console.log(acceptedSongs);
+        displaySongs(acceptedSongs);
+    } catch (error) {
+        console.error(error);
+        alert('Error al obtener las canciones aceptadas. Por favor, intente nuevamente.');
+    }
 }
 
+// Función para mostrar las canciones aceptadas
+function displaySongs(songs) {
+    const acceptedContainer = document.getElementById('song-container-accepted');
+    acceptedContainer.innerHTML = ""; // Limpiamos el contenedor antes de agregar los elementos nuevos
 
-function displaySong(song, container) {
-    const songItem = document.createElement('div');
-    songItem.className = 'song-item';
-
-    const songImage = document.createElement('div');
-    songImage.className = 'song-image';
-    const imgElement = document.createElement('img');
-    imgElement.src = song.image;
-    songImage.appendChild(imgElement);
-
-    const songDetails = document.createElement('div');
-    songDetails.className = 'song-details';
-    const songTitle = document.createElement('p');
-    songTitle.className = 'song-title';
-    songTitle.innerText = song.name;
-    const songArtist = document.createElement('p');
-    songArtist.className = 'song-artist';
-    songArtist.innerText = song.artist.name;
-
-    songDetails.appendChild(songTitle);
-    songDetails.appendChild(songArtist);
-    songItem.appendChild(songImage);
-    songItem.appendChild(songDetails);
-
-    container.appendChild(songItem);
+    songs.forEach(song => {
+        const songElement = document.createElement('div');
+        songElement.className = 'song-item';
+        songElement.innerHTML = `
+            <div class="song-image"><img src="${song.image_url}" alt="${song.name}"></div>
+            <div class="song-details">
+                <p class="song-title">${song.name}</p>
+                <p class="song-artist">${song.artist}</p>
+            </div>
+        `;
+        acceptedContainer.appendChild(songElement);
+    });
 }
+
+// Inicializar la obtención de canciones aceptadas
+document.addEventListener('DOMContentLoaded', () => getSelectedSongs(party_id));
